6c64a4c6096ee8e61c3f3dfca8f2b11bcf4d8ad9
enzosdev <enzohfs@gmail.com>
2026-01-12 20:50:59 -0300

    chore: Initial project setup

---
+++ src/main/java/enzosdev/elifoot/ElifootApplication.java
@@ -0,0 +1,13 @@
+package enzosdev.elifoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElifootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElifootApplication.class, args);
+    }
+
+}
