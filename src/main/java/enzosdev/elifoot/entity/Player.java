5637056fe8af9cd5f17d4b26d45ecd24bd896282
enzosdev <enzohfs@gmail.com>
2026-01-13 19:50:48 -0300

    feat: add Player entity with Many to One relation with clubs

--- src/main/java/enzosdev/elifoot/entity/Player.java
+++ src/main/java/enzosdev/elifoot/entity/Player.java
@@ -1,4 +1,30 @@
 package enzosdev.elifoot.entity;
 
+import enzosdev.elifoot.enums.Position;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
 public class Player {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Position position;
+    private Integer shirtNumber;
+    private String urlImg;
+
+    @ManyToOne
+    @JoinColumn(name = "club_id")
+    private Club club;
+
 }
