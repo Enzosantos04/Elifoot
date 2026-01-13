5ed842e520bb29e90387df4af429f0c8a41c2e37
enzosdev <enzohfs@gmail.com>
2026-01-13 19:49:49 -0300

    feat: add club entity with one to one relation with stadium and one to many relation with players

--- src/main/java/enzosdev/elifoot/entity/Club.java
+++ src/main/java/enzosdev/elifoot/entity/Club.java
@@ -1,4 +1,33 @@
 package enzosdev.elifoot.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
 public class Club {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private LocalDate founded;
+    private String urlImg;
+
+    @OneToOne
+    @JoinColumn(name = "stadium_id")
+    private Stadium stadium;
+
+    @OneToMany
+    private List<Player> players;
+
+
 }
