b2c2d70c616d11c24f07f4ef23cb300b4482db34
enzosdev <enzohfs@gmail.com>
2026-01-13 19:53:00 -0300

    feat: enum for player position

--- src/main/java/enzosdev/elifoot/enums/Position.java
+++ src/main/java/enzosdev/elifoot/enums/Position.java
@@ -1,4 +1,20 @@
 package enzosdev.elifoot.enums;
 
 public enum Position {
+    GOALKEEPER("Goalkeeper"),
+    DEFENDER("Defender"),
+    FULLBACK("Fullback"),
+    MIDFIELDER("Midfielder"),
+    FORWARD("Forward");
+
+
+    private final String label;
+
+    Position(String label){
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 }
