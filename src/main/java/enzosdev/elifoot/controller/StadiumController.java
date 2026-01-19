088772403b4eaec498e192e5f16e0d75aaa87739
enzosdev <enzohfs@gmail.com>
2026-01-19 19:52:12 -0300

    feat: add stadium controller

---
+++ src/main/java/enzosdev/elifoot/controller/StadiumController.java
@@ -0,0 +1,10 @@
+package enzosdev.elifoot.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stadiums")
+public class StadiumController {
+}
