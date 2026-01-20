ff519db4ffb25d2fbd7900789d1b9173b4805f93
enzosdev <enzohfs@gmail.com>
2026-01-19 22:26:47 -0300

    feat: add a simple findAll stadiums endpoint

--- src/main/java/enzosdev/elifoot/controller/StadiumController.java
+++ src/main/java/enzosdev/elifoot/controller/StadiumController.java
@@ -1,10 +1,27 @@
 package enzosdev.elifoot.controller;
 
 
+import enzosdev.elifoot.dto.StadiumDTO;
+import enzosdev.elifoot.service.StadiumService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/stadiums")
 public class StadiumController {
+
+
+    private final StadiumService stadiumService;
+
+    public StadiumController(StadiumService stadiumService) {
+        this.stadiumService = stadiumService;
+    }
+
+    @GetMapping
+    public Page<StadiumDTO> findAll(Pageable pageable){
+        return stadiumService.findAll(pageable);
+    }
 }
