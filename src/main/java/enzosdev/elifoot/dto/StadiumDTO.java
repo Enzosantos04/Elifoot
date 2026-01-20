22c02c17f5b188bc2b14ad8c5ab2db82387419d9
enzosdev <enzohfs@gmail.com>
2026-01-19 22:01:49 -0300

    feat: add annotations and build the dto

--- src/main/java/enzosdev/elifoot/dto/StadiumDTO.java
+++ src/main/java/enzosdev/elifoot/dto/StadiumDTO.java
@@ -1,6 +1,26 @@
 package enzosdev.elifoot.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class StadiumDTO {
 
+
+    private Long id;
+
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String city;
+
+    private Integer capacity;
+    @JsonInclude(value = JsonInclude.Include.NON_NULL)
+    private String urlImg;
 
 }
