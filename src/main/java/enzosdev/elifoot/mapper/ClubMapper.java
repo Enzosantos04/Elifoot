eda4eae2b52fcb1776b492273279d9f8af57fd96
enzosdev <enzohfs@gmail.com>
2026-01-21 20:48:06 -0300

    feat: add mapper for club dto and club entity

--- src/main/java/enzosdev/elifoot/mapper/ClubMapper.java
+++ src/main/java/enzosdev/elifoot/mapper/ClubMapper.java
@@ -1,8 +1,31 @@
 package enzosdev.elifoot.mapper;
 
 
+import enzosdev.elifoot.dto.ClubDTO;
+import enzosdev.elifoot.entity.Club;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ClubMapper {
+
+    public ClubDTO map(Club club){
+        ClubDTO dto = new ClubDTO();
+        dto.setId(club.getId());
+        dto.setName(club.getName());
+        dto.setFounded(club.getFounded());
+        dto.setUrlImg(club.getUrlImg());
+
+        return dto;
+    }
+
+
+    public Club map(ClubDTO clubDTO){
+        Club club = new Club();
+        club.setId(club.getId());
+        club.setName(club.getName());
+        club.setFounded(club.getFounded());
+        club.setUrlImg(clubDTO.getUrlImg());
+
+        return club;
+    }
 }
