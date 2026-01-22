c880b99a107bb566bc8a8b654489cab3f1e4deae
enzosdev <enzohfs@gmail.com>
2026-01-21 21:02:34 -0300

    feat: add findAll clubs endpoint

--- src/main/java/enzosdev/elifoot/controller/ClubController.java
+++ src/main/java/enzosdev/elifoot/controller/ClubController.java
@@ -1,9 +1,25 @@
 package enzosdev.elifoot.controller;
 
+import enzosdev.elifoot.dto.ClubDTO;
+import enzosdev.elifoot.service.ClubService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/clubs")
 public class ClubController {
+
+    private final ClubService clubService;
+
+    public ClubController(ClubService clubService) {
+        this.clubService = clubService;
+    }
+
+    @GetMapping
+    public Page<ClubDTO> findAll(Pageable pageable){
+        return clubService.findAll(pageable);
+    }
 }
