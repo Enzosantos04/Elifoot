d56a5678d6b259ed847eb4dc655066719ffd42c1
enzosdev <enzohfs@gmail.com>
2026-01-21 21:04:16 -0300

    feat: add findAll club BRs

--- src/main/java/enzosdev/elifoot/service/ClubService.java
+++ src/main/java/enzosdev/elifoot/service/ClubService.java
@@ -1,8 +1,28 @@
 package enzosdev.elifoot.service;
 
 
+import enzosdev.elifoot.dto.ClubDTO;
+import enzosdev.elifoot.mapper.ClubMapper;
+import enzosdev.elifoot.repository.ClubRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClubService {
+
+    private final ClubRepository clubRepository;
+    private final ClubMapper clubMapper;
+
+    public ClubService(ClubRepository clubRepository, ClubMapper clubMapper) {
+        this.clubRepository = clubRepository;
+        this.clubMapper = clubMapper;
+    }
+
+
+
+    public Page<ClubDTO> findAll(Pageable pageable){
+     return clubRepository.findAll(pageable)
+             .map(clubMapper::map);
+    }
 }
