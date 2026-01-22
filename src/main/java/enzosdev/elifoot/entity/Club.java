46515ba1c3fbbd970579b17c8333117294226cb7
enzosdev <enzohfs@gmail.com>
2026-01-21 21:26:13 -0300

    feat: add @JsonIgnore annotation to avoid serialization error

--- src/main/java/enzosdev/elifoot/entity/Club.java
+++ src/main/java/enzosdev/elifoot/entity/Club.java
@@ -1,5 +1,6 @@
 package enzosdev.elifoot.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -27,6 +28,7 @@ public class Club {
     private Stadium stadium;
 
     @OneToMany
+    @JsonIgnore
     private List<Player> players;
 
 
