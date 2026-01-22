8a99c05d48124a45a499f401e612e7fc62a69477
enzosdev <enzohfs@gmail.com>
2026-01-21 21:36:02 -0300

    feat: add findClubById method as BRs

--- src/main/java/enzosdev/elifoot/service/ClubService.java
+++ src/main/java/enzosdev/elifoot/service/ClubService.java
@@ -2,11 +2,14 @@ package enzosdev.elifoot.service;
 
 
 import enzosdev.elifoot.dto.ClubDTO;
+import enzosdev.elifoot.entity.Club;
 import enzosdev.elifoot.mapper.ClubMapper;
 import enzosdev.elifoot.repository.ClubRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ClubService {
@@ -24,5 +27,12 @@ public class ClubService {
     public Page<ClubDTO> findAll(Pageable pageable){
      return clubRepository.findAll(pageable)
              .map(clubMapper::map);
+    }
+
+
+    public ClubDTO findClubById(Long id){
+        Optional<Club> club = clubRepository.findById(id);
+              return club.map(clubMapper::map)
+                      .orElseThrow(() -> new RuntimeException("Club Not found."));
     }
 }
