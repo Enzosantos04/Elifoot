2179945a79b94b285b3e2e6ade1d46e8e05b9fd0
enzosdev <enzohfs@gmail.com>
2026-01-24 22:36:41 -0300

    feat: player mapper for player dto and entity

--- src/main/java/enzosdev/elifoot/mapper/PlayerMapper.java
+++ src/main/java/enzosdev/elifoot/mapper/PlayerMapper.java
@@ -1,7 +1,36 @@
 package enzosdev.elifoot.mapper;
 
+import enzosdev.elifoot.dto.PlayerDTO;
+import enzosdev.elifoot.entity.Player;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PlayerMapper {
+
+    public PlayerDTO map(Player player){
+        PlayerDTO dto = new PlayerDTO();
+        dto.setId(player.getId());
+        dto.setName(player.getName());
+        dto.setPosition(player.getPosition());
+        dto.setShirtNumber(player.getShirtNumber());
+        dto.setUrlImg(player.getUrlImg());
+        dto.setClub(player.getClub());
+
+        return dto;
+
+    }
+
+
+    public Player map(PlayerDTO playerDTO){
+        Player player = new Player();
+        player.setId(playerDTO.getId());
+        player.setName(playerDTO.getName());
+        player.setPosition(playerDTO.getPosition());
+        player.setShirtNumber(playerDTO.getShirtNumber());
+        player.setUrlImg(playerDTO.getUrlImg());
+        player.setClub(playerDTO.getClub());
+
+        return player;
+
+    }
 }
