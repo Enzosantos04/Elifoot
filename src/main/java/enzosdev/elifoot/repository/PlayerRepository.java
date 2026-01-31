8f967905a876ae1ea590010d6be1448a60c0d2b1
enzosdev <enzohfs@gmail.com>
2026-01-31 13:17:28 -0300

    feat: add derived queries for player repo

--- src/main/java/enzosdev/elifoot/repository/PlayerRepository.java
+++ src/main/java/enzosdev/elifoot/repository/PlayerRepository.java
@@ -1,7 +1,12 @@
 package enzosdev.elifoot.repository;
 
+
 import enzosdev.elifoot.entity.Player;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PlayerRepository extends JpaRepository<Player, Long> {
+    List<Player> findPlayersByClubId(Long clubId);
 }
