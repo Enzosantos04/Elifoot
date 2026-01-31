030e5fbe3ce6a53d9bec345db3d1146f2b099855
enzosdev <enzohfs@gmail.com>
2026-01-30 22:20:01 -0300

    feat: add createPlayer endpoint

--- src/main/java/enzosdev/elifoot/controller/PlayerController.java
+++ src/main/java/enzosdev/elifoot/controller/PlayerController.java
@@ -1,9 +1,29 @@
 package enzosdev.elifoot.controller;
 
+import enzosdev.elifoot.dto.PlayerDTO;
+import enzosdev.elifoot.service.PlayerService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(name = "/players")
+@RequestMapping("/players")
 public class PlayerController {
+
+    private final PlayerService playerService;
+
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+
+    @PostMapping
+    public PlayerDTO createPlayer(@RequestBody PlayerDTO playerDTO){
+        return playerService.createPlayer(playerDTO);
+
+    }
+
+
+
 }
