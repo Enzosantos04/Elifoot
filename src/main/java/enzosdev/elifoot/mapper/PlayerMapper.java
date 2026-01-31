e3c2dbea5a5900be17200e00557587b12f37e185
enzosdev <enzohfs@gmail.com>
2026-01-31 13:16:49 -0300

    feat: add getter e setter for clubid

--- src/main/java/enzosdev/elifoot/mapper/PlayerMapper.java
+++ src/main/java/enzosdev/elifoot/mapper/PlayerMapper.java
@@ -1,6 +1,7 @@
 package enzosdev.elifoot.mapper;
 
 import enzosdev.elifoot.dto.PlayerDTO;
+import enzosdev.elifoot.entity.Club;
 import enzosdev.elifoot.entity.Player;
 import org.springframework.stereotype.Component;
 
@@ -9,11 +10,17 @@ public class PlayerMapper {
 
     public PlayerDTO map(Player player){
         PlayerDTO dto = new PlayerDTO();
+        dto.setId(player.getId());
         dto.setName(player.getName());
         dto.setPosition(player.getPosition());
         dto.setShirtNumber(player.getShirtNumber());
         dto.setUrlImg(player.getUrlImg());
+        if(player.getClub() != null){
+            dto.setClubId(player.getClub().getId());
+
+        }
         dto.setClub(player.getClub());
+
 
         return dto;
 
@@ -22,11 +29,17 @@ public class PlayerMapper {
 
     public Player map(PlayerDTO playerDTO){
         Player player = new Player();
+        player.setId(playerDTO.getId());
         player.setName(playerDTO.getName());
         player.setPosition(playerDTO.getPosition());
         player.setShirtNumber(playerDTO.getShirtNumber());
         player.setUrlImg(playerDTO.getUrlImg());
-        player.setClub(playerDTO.getClub());
+        if (playerDTO.getClubId() != null) {
+            Club club = new Club();
+            club.setId(playerDTO.getClubId());
+            player.setClub(club);
+        }
+
 
         return player;
 
