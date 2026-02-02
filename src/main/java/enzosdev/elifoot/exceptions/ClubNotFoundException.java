30ad92165835181914a1f524b20606523a5b5b43
enzosdev <enzohfs@gmail.com>
2026-02-01 21:42:14 -0300

    feat: add club not found exception

---
+++ src/main/java/enzosdev/elifoot/exceptions/ClubNotFoundException.java
@@ -0,0 +1,7 @@
+package enzosdev.elifoot.exceptions;
+
+public class ClubNotFoundException extends RuntimeException {
+    public ClubNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/enzosdev/elifoot/exceptions/PlayerNotFoundException.java
@@ -0,0 +1,7 @@
+package enzosdev.elifoot.exceptions;
+
+public class PlayerNotFoundException extends RuntimeException {
+  public PlayerNotFoundException(String message) {
+    super(message);
+  }
+}
