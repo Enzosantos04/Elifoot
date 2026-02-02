7a98c80f8b1e21bbfe6e4077bf2e1a2ead56aa24
enzosdev <enzohfs@gmail.com>
2026-02-02 20:42:54 -0300

    feat: add corsconfig to allow cross-origin

--- src/main/java/enzosdev/elifoot/config/CorsConfig.java
+++ src/main/java/enzosdev/elifoot/config/CorsConfig.java
@@ -1,6 +1,22 @@
 package enzosdev.elifoot.config;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsConfig {
+
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhost:3000");
+        corsConfiguration.addAllowedHeader("GET, POST, PUT, DELETE, PATCH, OPTIONS");
+        corsConfiguration.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+
+    }
+
+
 }
