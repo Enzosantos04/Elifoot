8acd741291be08c1532bff13d1ac821f908945b1
enzosdev <enzohfs@gmail.com>
2026-02-03 19:43:20 -0300

    feat: add exception handling for player not found

--- src/main/java/enzosdev/elifoot/exceptions/ControllerExceptionHandler.java
+++ src/main/java/enzosdev/elifoot/exceptions/ControllerExceptionHandler.java
@@ -1,4 +1,24 @@
 package enzosdev.elifoot.exceptions;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class ControllerExceptionHandler {
+
+
+    @ExceptionHandler(PlayerNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handlePlayerNotFoundException(PlayerNotFoundException ex){
+        Map<String, String> response  = new HashMap<>();
+        response.put("error", ex.getMessage());
+        response.put("message", "Player not found");
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+
+    }
 }
