a5c986b71d29448e65e5ca2172ce9fb79f649ba7
enzosdev <enzohfs@gmail.com>
2026-02-03 20:58:45 -0300

    feat: remove authentication for any endpoint

--- src/main/java/enzosdev/elifoot/config/SecurityConfig.java
+++ src/main/java/enzosdev/elifoot/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/users").permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(Customizer.withDefaults())
