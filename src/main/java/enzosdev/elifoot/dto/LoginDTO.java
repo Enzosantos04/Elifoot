4cc640309d552bffa0ef02fde4625bb3296c256b
enzosdev <enzohfs@gmail.com>
2026-02-09 19:33:25 -0300

    feat: add loginDTO

---
+++ src/main/java/enzosdev/elifoot/dto/LoginDTO.java
@@ -0,0 +1,11 @@
+package enzosdev.elifoot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class LoginDTO {
+}
