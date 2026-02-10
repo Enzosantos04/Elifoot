66f9bd46a6367cf4bc1103a33321be5b20c9b484
enzosdev <enzohfs@gmail.com>
2026-02-09 21:17:16 -0300

    feat: add scope repository

---
+++ src/main/java/enzosdev/elifoot/repository/ScopeRepository.java
@@ -0,0 +1,7 @@
+package enzosdev.elifoot.repository;
+
+import enzosdev.elifoot.entity.Scope;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScopeRepository extends JpaRepository<Scope, Long> {
+}
