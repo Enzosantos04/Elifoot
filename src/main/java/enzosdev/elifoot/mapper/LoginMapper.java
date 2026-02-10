2fbce55225fefab1ae02da1cfd9e12b95e8bafc9
enzosdev <enzohfs@gmail.com>
2026-02-10 18:05:02 -0300

    feat: add LoginMapper to convert User entity to DTO

--- src/main/java/enzosdev/elifoot/mapper/LoginMapper.java
+++ src/main/java/enzosdev/elifoot/mapper/LoginMapper.java
@@ -1,7 +1,24 @@
 package enzosdev.elifoot.mapper;
 
+import enzosdev.elifoot.dto.LoginDTO;
+import enzosdev.elifoot.entity.User;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LoginMapper {
+
+    public LoginDTO map(User user){
+        LoginDTO loginDTO = new LoginDTO();
+        loginDTO.setId(user.getId());
+        loginDTO.setEmail(user.getEmail());
+        return loginDTO;
+    }
+
+
+    public User map(LoginDTO loginDTO){
+        User user = new User();
+        user.setId(loginDTO.getId());
+        user.setEmail(loginDTO.getEmail());
+        return user;
+    }
 }
