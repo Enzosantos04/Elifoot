a41816062c14b6a4d1037e3c143002218e0e69a1
enzosdev <enzohfs@gmail.com>
2026-02-10 22:02:16 -0300

    feat: add login endpoint

--- src/main/java/enzosdev/elifoot/controller/LoginController.java
+++ src/main/java/enzosdev/elifoot/controller/LoginController.java
@@ -1,10 +1,27 @@
 package enzosdev.elifoot.controller;
 
 
+import enzosdev.elifoot.dto.LoginDTO;
+import enzosdev.elifoot.service.LoginService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/login")
 public class LoginController {
+
+
+    private final LoginService loginService;
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+
+    @PostMapping
+    public LoginDTO login(@RequestBody LoginDTO loginDTO){
+        return loginService.login(loginDTO);
+    }
 }
