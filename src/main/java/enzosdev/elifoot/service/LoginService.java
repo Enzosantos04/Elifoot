3e9379650228bee259916aa3c6fabd8c4ae53c2e
enzosdev <enzohfs@gmail.com>
2026-02-10 22:06:16 -0300

    feat: implement JWT token generation and login flow

--- src/main/java/enzosdev/elifoot/service/LoginService.java
+++ src/main/java/enzosdev/elifoot/service/LoginService.java
@@ -1,12 +1,17 @@
 package enzosdev.elifoot.service;
 
 import enzosdev.elifoot.dto.LoginDTO;
+import enzosdev.elifoot.entity.Scope;
 import enzosdev.elifoot.entity.User;
 import enzosdev.elifoot.repository.UserRepository;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,11 +20,13 @@ public class LoginService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtEncoder jwtEncoder;
 
 
-    public LoginService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public LoginService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtEncoder jwtEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtEncoder = jwtEncoder;
     }
 
 
@@ -28,7 +35,28 @@ public class LoginService {
         if (optionalUser.isEmpty() || !isPasswordValid(loginDTO.getPassword(), optionalUser.get().getPassword())) {
             throw new BadCredentialsException("Invalid email or password");
         }
-        return loginDTO;
+
+
+
+        User savedUser = optionalUser.get();
+        List<String> scopes = savedUser.getScopes().stream()
+                .map(Scope::getName)
+                .toList();
+
+        long expiresIn = 600L;
+        JwtClaimsSet jwtClaims = JwtClaimsSet.builder()
+                .issuer("elifoot")
+                .expiresAt(Instant.now().plusSeconds(expiresIn))
+                .subject( savedUser.getEmail())
+                .claim("scopes", scopes)
+                .claim("email", savedUser.getEmail())
+                .build();
+        String token = jwtEncoder.encode(JwtEncoderParameters.from(jwtClaims)).getTokenValue();
+
+        return LoginDTO.builder()
+                .email(savedUser.getEmail())
+                .token(token)
+                .build();
     }
 
     private boolean isPasswordValid(String password, String savedPassword){
