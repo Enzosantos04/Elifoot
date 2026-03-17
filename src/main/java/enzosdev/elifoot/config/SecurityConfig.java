e26a5527420f3d4e1ac51398c844702d9979b91b
enzosdev <enzohfs@gmail.com>
2026-03-17 10:42:52 -0300

    fix: add missing import for EnableMethodSecurity annotation

--- src/main/java/enzosdev/elifoot/config/SecurityConfig.java
+++ src/main/java/enzosdev/elifoot/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -29,7 +30,7 @@ import java.security.interfaces.RSAPublicKey;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
     @Value("${jwt.public.key}")
